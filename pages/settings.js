67a1dada0dfb6ace3ae33d752065dec3ba5f3194
Ahmet Ege Sandal <ahmetegesandal94@gmail.com>
2025-03-10 18:48:57 +0300

    settings sayfası eklendi.

---
+++ pages/settings.js
@@ -0,0 +1,30 @@
+import LayoutMenu from "../components/LayoutMenu";
+import Navbar from "../components/Navbar";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+const Settings = () => {
+  const { t } = useTranslation("common");
+
+  return (
+    <>
+      <LayoutMenu />
+      <div className="layout-page">
+        <Navbar />
+        <div className="content-wrapper">
+          <h1 className="text-4xl font-bold mb-6">Ayarlar</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
+
+export default Settings;
